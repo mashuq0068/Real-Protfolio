1b5f537d942fdfe07d295384ee5a6b4be666a36d
mashuq0068 <mashuq0068@gmail.com>
2023-10-28 14:54:59 +0600

    added the info

--- src/Components/Banner/Baneer.jsx
+++ src/Components/Banner/Baneer.jsx
@@ -11,7 +11,7 @@ const Banner = () => {
     return (
         <>
         <div className=" flex justify-between font-nosifer ">
-        <div className="flex flex-col  font-custom font-thin glass w-[50%] text-2xl  lg:text-3xl  xl:text-4xl  2xl:text-5xl text-gray-700   pl-[6%] justify-center">
+        <div className="flex flex-col  font-custom font-thin glass md:text-4xl lg:w-[50%] text-2xl  lg:text-3xl  xl:text-4xl  2xl:text-5xl text-gray-700   pl-[6%] justify-center">
         <div  data-aos="fade-right"  data-aos-easing="linear"
      data-aos-duration="1500"className=''>
       <p> Hi! I am  Mashuq, </p>
@@ -21,13 +21,13 @@ const Banner = () => {
        
        
        <div  data-aos="zoom-in-up" data-aos-duration="1500">
-            <button data-aos="zoom-in-up" className=' px-8 py-3 mt-8 lg:mt-12  xl:mt-16  2xl:mt-24 hover:font-bold hover:pr-12 duration-1000 text-base  border xl:text-xl 2xl:text-2xl drop-shadow-xl  shadow-xl border-slate-400' >Get Started</button>
+            <button data-aos="zoom-in-up" className='px-2 py-1 lg:px-8 lg:py-3 mt-8 lg:mt-12  xl:mt-16  2xl:mt-24 hover:font-bold hover:pr-12 duration-1000 text-base  border xl:text-xl 2xl:text-2xl drop-shadow-xl  shadow-xl border-slate-400' >Get Started</button>
           </div>
            </div>
         <div className=" flex ">
       
          
-          <a className="myAni" href="https://ibb.co/jyBYX7s"><img className="w-[23vw]    h-[100vh]  " src="https://i.ibb.co/BydJM8F/m44-removebg-preview.png" alt="m44-remove bg-preview" /></a>
+          <a className="myAni" href="https://ibb.co/jyBYX7s"><img className="lg:w-[23vw] h-[90vh]   lg:h-[100vh]  " src="https://i.ibb.co/BydJM8F/m44-removebg-preview.png" alt="m44-remove bg-preview" /></a>
          
          
        
--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -2,9 +2,32 @@
 
 const Info = () => {
     return (
-        <div className="myAni bg-slate-200 h-screen">
-            This is info page
+        <div className="h-screen bg-white">
+        
+         <div className="flex gap-[2%] justify-center pt-[5%] bg-slate-200 ">
+         <p className="2xl:w-[40px] w-[20px] h-[20px] lg:w-[30px] ani-bounce bg-rose-500 lg:h-[30px] 2xl:h-[40px] rounded-[50%]"></p>
+         <p className="2xl:w-[40px] w-[20px] h-[20px] lg:w-[30px] ani-bounce2 bg-amber-500 lg:h-[30px] 2xl:h-[40px] rounded-[50%]"></p>
+         <p className="2xl:w-[40px] w-[20px] h-[20px] lg:w-[30px] ani-bounce3 bg-teal-500 lg:h-[30px] 2xl:h-[40px] rounded-[50%]"></p>
+         <p className="2xl:w-[40px] w-[20px] h-[20px] lg:w-[30px] ani-bounce4  bg-purple-500 lg:h-[30px] 2xl:h-[40px] rounded-[50%]"></p>
+        
+     </div>
+    
+    <div className="flex-col flex items-center justify-center pb-[5%] bg-slate-200 ">
+        <div className="relative ani-left z-20 xl:text-2xl   2xl:text-3xl p-[2%] bg-glass top-5 drop-shadow-xl " >
+           <h2>About my Info</h2>
         </div>
+        <div  className="ani-right p-[3%] lg:pl-[3%] xl:text-xl overflow-y-scroll lg:h-[70vh] 2xl:text-2xl ">
+            <p className=" lg:w-[35vw] text-center
+              p-[5%] bg-glass ani-letter  drop-shadow-xl  font-medium
+              ">
+            I am an accomplished front-end developer with a deep proficiency in HTML, CSS, and JavaScript, along with a specialization in React. I am passionate about crafting engaging and user-friendly web interfaces. My dedication to elegant design, combined with my mastery of these technologies, enables me to transform creative concepts into responsive and visually stunning websites. My work marries creative aesthetics with smooth functionality, ensuring that every project I undertake achieves excellence in code and user experience. Explore my portfolio to witness the seamless fusion of creativity and code, with React as my forte.
+            </p>
+        </div>
+        </div>
+       
+        
+       
+    </div>
     );
 };
 
